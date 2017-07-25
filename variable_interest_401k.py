42a880f0119e2f997fd7a0702c4993ee044e8566
rbirchtree <rbirchtree@gmail.com>
2017-07-25 11:38:06 -0500

    some sort of commit message

--- variable_interest_401k.py
+++ variable_interest_401k.py
@@ -1,6 +1,6 @@
 contribution = int(raw_input("How much do you want to contribute to your 401k per a year?"))
 # money contributed to 401k
-wax_ball = int(raw_input("What is your 401k worth now?"))
+waxBall = int(raw_input("What is your 401k worth now?"))
 # sum of all contribution plus interest of every year
 time= int(raw_input("How many years from now do you want to withdraw on it?"))
 # time in years
@@ -10,17 +10,17 @@ y = float(raw_input())
 z = float(raw_input())
 interest_rate = [x,y,z]
 #average return rate randomized
-
+#fix variable names
 from random import choice
 import locale
 locale.setlocale( locale.LC_ALL, '')
 'English_United States.1252'
 choice(interest_rate)
 for x in range (0,time):
-    wax_ball= contribution+(wax_ball*(1+choice(interest_rate)))
-print locale.currency(wax_ball)
+    wax_ball= contribution+(waxBall*(1+choice(interest_rate)))
+print locale.currency(waxBall)
 
-if wax_ball > 2000000:
+if waxBall > 2000000:
 	print "Great saving"
-elif wax_ball < 2000000:
+elif waxBall < 2000000:
 	print "Save more, spend less and/or earn more."
